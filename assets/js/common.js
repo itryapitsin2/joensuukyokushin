9ac87441262c8eaeb43b008d2b38406b969779cf
itryapitsin <iliya.tryapitsin@gmail.com>
2019-12-02 14:36:14 +0200

    Add map

--- assets/js/common.js
+++ assets/js/common.js
@@ -8,3 +8,30 @@ function toggleBackground() {
     }
 
 }
+
+var OneSignal = window.OneSignal || [];
+OneSignal.push(function () {
+    OneSignal.init({
+        appId: "ccc9dfda-32e7-4856-9b6a-e14865466f59",
+    });
+});
+
+ymaps.ready(init);
+
+function init() {
+    var myMap = new ymaps.Map("map", {
+        center: [62.602448, 29.7524578],
+        zoom: 16,
+        controls: ['zoomControl']
+    });
+
+    var myGeoObject = new ymaps.GeoObject({
+        // Описание геометрии.
+        geometry: {
+            type: "Point",
+            coordinates: [62.602448, 29.7524578]
+        },
+    });
+
+    myMap.geoObjects.add(myGeoObject);
+}
